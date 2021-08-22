80c3b18b2360de213bcb80b6d5cdf29699d10d02
Cathrach <serina@fractal.co>
2021-08-21 23:06:59 -0400

    No more mac memcpy (#16)
    * preliminray changes
    
    * Reinstated NULL checks
    
    * indicate that UVplane will go unused
    
    * software check
    
    only format my if statement
    
    delete empty line
    
    * modify the README
    
    * fix dependency

--- src/render/metal/SDL_render_metal.m
+++ src/render/metal/SDL_render_metal.m
@@ -30,6 +30,7 @@
 #include <Availability.h>
 #import <Metal/Metal.h>
 #import <QuartzCore/CAMetalLayer.h>
+#import <VideoToolbox/VideoToolbox.h>
 
 #ifdef __MACOSX__
 #import <AppKit/NSView.h>
@@ -819,6 +820,7 @@ METAL_UpdateTextureYUV(SDL_Renderer * renderer, SDL_Texture * texture,
      */
     METAL_RenderData *data = (__bridge METAL_RenderData *) renderer->driverdata;
     METAL_TextureData *texturedata = (__bridge METAL_TextureData *)texture->driverdata;
+
     const int Uslice = 0;
     const int Vslice = 1;
     SDL_Rect UVrect = {rect->x / 2, rect->y / 2, (rect->w + 1) / 2, (rect->h + 1) / 2};
@@ -832,7 +834,6 @@ METAL_UpdateTextureYUV(SDL_Renderer * renderer, SDL_Texture * texture,
     if (Upitch != Vpitch) {
         return -1;
     }
-  
     // Check whether the texture has data
     if (!texturedata.hasdata && METAL_GetStorageMode(texturedata.mtltexture) != MTLStorageModePrivate
             && METAL_GetStorageMode(texturedata.mtltexture_uv) != MTLStorageModePrivate) {
@@ -842,23 +843,23 @@ METAL_UpdateTextureYUV(SDL_Renderer * renderer, SDL_Texture * texture,
         METAL_UploadTextureData(texturedata.mtltexture_uv, UVrect, Vslice, Vplane, Vpitch);
         return 0;
     }
-  
+    
     // Get the length of the texture data
     int totalLength = rect->h * Ypitch + UVrect.h * (Upitch + Vpitch);
     int YdataLength= rect->h * Ypitch;
     int UdataLength = UVrect.h * Upitch;
-  
+    
     // Ensure that the YUV data is continuous
     if (Yplane + YdataLength != Uplane || Uplane + UdataLength != Vplane) {
         return -1;
     }
-
+    
     // Make a buffer that wraps the data we've gotten
     id<MTLBuffer> dataBuffer = [data.mtldevice newBufferWithBytesNoCopy:Yplane
                                                                  length:totalLength
                                                                 options:MTLResourceStorageModeShared
                                                             deallocator:nil];
-  
+    
     // We need to make two staging textures: one for Y and one for UV, this make the texture descriptors
     MTLTextureDescriptor *Ydesc = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:texturedata.mtltexture.pixelFormat
                                                                                      width:rect->w
@@ -868,27 +869,26 @@ METAL_UpdateTextureYUV(SDL_Renderer * renderer, SDL_Texture * texture,
                                                                                       width:UVrect.w
                                                                                      height:UVrect.h
                                                                                   mipmapped:NO];
-  
+    
     // Check that the descriptors are initialized
     if (Ydesc == nil || UVdesc == nil) {
         return -1;
     }
-
+    
     // Make the staging texture from dataBuffer
     id<MTLTexture> Ystagingtex = [dataBuffer newTextureWithDescriptor:Ydesc
                                                                offset:0
                                                           bytesPerRow:Ypitch];
-  
+    
     // We verified above that Upitch = Vpitch
     id<MTLTexture> UVstagingtex = [dataBuffer newTextureWithDescriptor:UVdesc
                                                                 offset:YdataLength
                                                            bytesPerRow:Upitch];
-  
+    
     // Check that the textures are initialized
     if (Ystagingtex == nil || UVstagingtex == nil) {
         return -1;
     }
-
     // Set up the command encoder and buffer, which queue events (copies for us) for GPU to execute
     if (data.mtlcmdencoder != nil) {
         [data.mtlcmdencoder endEncoding];
@@ -949,25 +949,77 @@ METAL_UpdateTextureNV(SDL_Renderer * renderer, SDL_Texture * texture,
                     const Uint8 *Yplane, int Ypitch,
                     const Uint8 *UVplane, int UVpitch)
 { @autoreleasepool {
+    METAL_RenderData *data = (__bridge METAL_RenderData *) renderer->driverdata;
     METAL_TextureData *texturedata = (__bridge METAL_TextureData *)texture->driverdata;
     SDL_Rect UVrect = {rect->x / 2, rect->y / 2, (rect->w + 1) / 2, (rect->h + 1) / 2};
+    if (Yplane == UVplane) {
+        // Fractal gave us VideoToolbox frame
 
-    /* Bail out if we're supposed to update an empty rectangle */
-    if (rect->w <= 0 || rect->h <= 0) {
+        /* Bail out if we're supposed to update an empty rectangle */
+        if (rect->w <= 0 || rect->h <= 0) {
+            return 0;
+        }
+
+        CVPixelBufferRef frame_data = (CVPixelBufferRef) Yplane;
+        CVMetalTextureCacheRef texture_cache;
+        CVMetalTextureCacheCreate(kCFAllocatorDefault, NULL, data.mtldevice, NULL, &texture_cache);
+        CVMetalTextureRef cv_y_texture;
+        CVMetalTextureRef cv_uv_texture;
+        CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault, texture_cache, frame_data, NULL, MTLPixelFormatR8Unorm, rect->w, rect->h, 0, &cv_y_texture);
+        CVMetalTextureCacheCreateTextureFromImage(kCFAllocatorDefault, texture_cache, frame_data, NULL, MTLPixelFormatRG8Unorm, UVrect.w, UVrect.h, 1, &cv_uv_texture);
+        id<MTLTexture> y_texture = CVMetalTextureGetTexture(cv_y_texture);
+        id<MTLTexture> uv_texture = CVMetalTextureGetTexture(cv_uv_texture);
+        if (data.mtlcmdencoder != nil) {
+            [data.mtlcmdencoder endEncoding];
+            data.mtlcmdencoder = nil;
+        }
+
+        // Queue a new buffer as needed
+        if (data.mtlcmdbuffer == nil) {
+            data.mtlcmdbuffer = [data.mtlcmdqueue commandBuffer];
+        }
+
+        // Queue up all the texture copies
+        id<MTLBlitCommandEncoder> blitcmd = [data.mtlcmdbuffer blitCommandEncoder];
+
+        // Copy the textures over
+        [blitcmd copyFromTexture:y_texture
+                     sourceSlice:0
+                     sourceLevel:0
+                    sourceOrigin:MTLOriginMake(0, 0, 0)
+                      sourceSize:MTLSizeMake(rect->w, rect->h, 1)
+                       toTexture:texturedata.mtltexture
+                destinationSlice:0
+                destinationLevel:0
+               destinationOrigin:MTLOriginMake(rect->x, rect->y, 0)];
+
+        [blitcmd copyFromTexture:uv_texture
+                     sourceSlice:0
+                     sourceLevel:0
+                    sourceOrigin:MTLOriginMake(0, 0, 0)
+                      sourceSize:MTLSizeMake(UVrect.w, UVrect.h, 1)
+                       toTexture:texturedata.mtltexture_uv
+                destinationSlice:0
+                destinationLevel:0
+               destinationOrigin:MTLOriginMake(UVrect.x, UVrect.y, 0)];
+
+        [blitcmd endEncoding];
+
+        [data.mtlcmdbuffer commit];
+        data.mtlcmdbuffer = nil;
+        texturedata.hasdata = YES;
+
+        return 0;
+    } else {
+        // ordinary software stuff
+        if (METAL_UpdateTextureInternal(renderer, texturedata, texturedata.mtltexture, *rect, 0, Yplane, Ypitch) < 0) {
+            return -1;
+        }
+        if (METAL_UpdateTextureInternal(renderer, texturedata, texturedata.mtltexture_uv, UVrect, 0, UVplane, UVpitch) < 0) {
+            return -1;
+        }
         return 0;
     }
-
-    if (METAL_UpdateTextureInternal(renderer, texturedata, texturedata.mtltexture, *rect, 0, Yplane, Ypitch) < 0) {
-        return -1;
-    }
-
-    if (METAL_UpdateTextureInternal(renderer, texturedata, texturedata.mtltexture_uv, UVrect, 0, UVplane, UVpitch) < 0) {
-        return -1;
-    }
-
-    texturedata.hasdata = YES;
-
-    return 0;
 }}
 #endif
 
