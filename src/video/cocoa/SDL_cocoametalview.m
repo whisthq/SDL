70003d7829d300ab10f5cb11ec6d23dddf87eb7c
Karthick <j.karthic2004@gmail.com>
2022-05-12 18:39:57 +0530

    Change the color space from SRGB to Adobe RGB (#48)

--- src/video/cocoa/SDL_cocoametalview.m
+++ src/video/cocoa/SDL_cocoametalview.m
@@ -79,12 +79,11 @@ SDL_MetalViewEventWatch(void *userdata, SDL_Event *event)
     // return [self.class.layerClass layer];
     // Added:
     CAMetalLayer* metalLayer = [CAMetalLayer new];
-    metalLayer.wantsExtendedDynamicRangeContent = YES;
     
     // List of color spaces at https://developer.apple.com/documentation/coregraphics/cgcolorspace/color_space_names?language=objc
     // Or, you can do colorspace = [[[NSScreen mainScreen] colorSpace] CGColorSpace]; for the current display's color space
-    // There are a variety of other ways to generate CGColorSpaces    
-    CGColorSpaceRef colorspace = CGColorSpaceCreateWithName(kCGColorSpaceExtendedSRGB);
+    // There are a variety of other ways to generate CGColorSpaces
+    CGColorSpaceRef colorspace = CGColorSpaceCreateWithName(kCGColorSpaceAdobeRGB1998);
     metalLayer.colorspace = colorspace;
     CGColorSpaceRelease(colorspace);
     return metalLayer;
